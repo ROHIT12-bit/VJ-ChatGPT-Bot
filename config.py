be5f4b102481c922ca02995feb40d7d048c9cfa9
ROHIT12-bit <reddyrohit823@gmail.com>
2025-06-14 19:21:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,12 +10,12 @@ def is_enabled(value, default):
     else:
         return default
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-ADMINS = int(environ.get("ADMINS", ""))
-DB_URI = environ.get("DB_URI", "")
+API_ID = int(environ.get("API_ID", "20366634"))
+API_HASH = environ.get("API_HASH", "72095ec36984aa9ceb0dbaa9cec31559")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7242758381:AAGkWrJ1z_0Jrmggh-Bo2D_6036Xj92NX4w")
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002611038436"))
+ADMINS = int(environ.get("ADMINS", "7845335174"))
+DB_URI = environ.get("DB_URI", "mongodb+srv://rohitreddyathuru:R6Co7MOjTYQOAqcq@cluster0.xrwjpl9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = environ.get("DB_NAME", "chatgptvjbot")
-OPENAI_API = environ.get("OPENAI_API", "")
+OPENAI_API = environ.get("OPENAI_API", "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")
 AI = is_enabled((environ.get("AI","True")), False)
